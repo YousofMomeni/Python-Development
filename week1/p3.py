73531fbe60f728bcf7475bf2cbf287d295cac257
yosofmomeni111@gmail.com <yosofmomeni111@gmail.com>
2025-01-22 20:33:21 +0330

    new

--- week1/p3.py
+++ week1/p3.py
@@ -1,2 +1,9 @@
+model = int(input('miladi(1) or shamsi(2)'))
 age = int(input('Enter your year of birth: '))
-print(1403-age)
+
+if model == 1:
+    print(2024-age)
+elif model == 2:
+    print(1403-age)
+else :
+    print('error')
